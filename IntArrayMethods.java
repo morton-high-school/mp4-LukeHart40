c099da733891c492523faadee01fa6ee089de4b0
LukeHart40 <lucas.hart@mcusd709.org>
2024-11-25 09:35:47 -0600

    Change 1
    Start

---
+++ IntArrayMethods.java
@@ -0,0 +1,33 @@
+public class IntArrayMethods{
+    public static int arraySum(int[] a){
+
+    }
+
+    public static double arrayMean(int[] a){
+
+    }
+
+    public static int arrayMin(int[] a){
+
+    }
+
+    public static int arrayMax(int[] a){
+
+    }
+
+    public static boolean[] arrayLocalMin(int[] a){
+
+    }
+
+    public static boolean[] arrayLocalMax(int[] a){
+
+    }
+
+    public static int arrayMode(int[] a){
+
+    }
+
+    public static int arrayCount(int[] a){
+        
+    }
+}
